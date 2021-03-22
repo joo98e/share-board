4f7cb15785f9ec185b08058e4f32c08c528fea30
jeongtb <joo98e@gmail.com>
2021-03-22 14:36:34 +0900

    serve

--- server.js
+++ server.js
@@ -8,12 +8,13 @@ const bodyParser = require('body-parser');
 // ********************************************************* Front
 // 업로드용
 const multer = require('multer');
+const { ESRCH } = require('constants');
 const upload = multer({dest : './upload'});
 
 // body-parser
 // REST API에서는 일반적으로 JSON 타입의 데이터를 주고 받는다.
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 // *********************************************************
 // ********************************************************* Back
 // db
@@ -21,7 +22,7 @@ const data = fs.readFileSync('./config/database.json');
 const infos = JSON.parse(data);
 
 // PORT
-const port = process.env.PORT || 6880;
+const port = process.env.PORT || 6868;
 
 
 const connection = mysql.createConnection({
@@ -36,17 +37,12 @@ connection.connect();
 
 // *********************************************************
 
+// 이미지 보기용 접근 경로
+app.use('/image', express.static('./upload'));
 // 회원가입
-app.post('/SignUp', upload.single('image'), (req, res) => {
-    // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
-    // 그것들을 DB에 넣어준다.
-
+app.post('/api/SignUp', upload.single('image'), (req, res) => {
     // 쿼리문
-    let sql = 'INSERT INTO users \
-    VALUES(null, ?, ?, ?, ?, ?, ?,)';
-
-    // parmas(column)
-    let parmas = [profileImage, user_id, user_pw, user_name, user_email, user_phone, user_gender];
+    let sql = 'INSERT INTO users(profileImage, user_id, user_pw, user_name, user_email, user_phone, user_gender) VALUES(null, ?, ?, ?, ?, ?, ?)';
 
     // bianry 데이터
     let profileImage = '/image/' + req.file.filename;
@@ -57,21 +53,32 @@ app.post('/SignUp', upload.single('image'), (req, res) => {
     let user_email = req.body.user_email;
     let user_phone = req.body.user_phone;
     let user_gender = req.body.user_gender;
-    
+
+    // parmas(column)
+    let parmas = [profileImage, user_id, user_pw, user_name, user_email, user_phone, user_gender];
+
+    console.log(profileImage);
+    console.log(user_id);
+    console.log(user_pw);
+    console.log(user_name);
+    console.log(user_email);
+    console.log(user_phone);
+    console.log(user_gender);
+
     connection.query(sql, parmas, (err, rows, fields) => {
         res.send(rows);
+        console.log(rows);
+        console.log(err);
     });
 
 });
 
-// 이미지 보기용 접근 경로
-app.use('/image', express.static('./upload'));
 
 // *********************************************************
 
 // 로그인
-app.get('/api/login', async (req, res) => {
-    const { user_id, user_pw } = req.body;
+app.get('/login', async (req, res) => {
+    let sql = ""    
 
     try {
         
